dc562f8b73efb56f9e870ad1897b23e1aaa98607
arpasree104 <arpasree104@gmail.com>
2026-07-06 00:19:45 +0700

    Add files via upload

--- src/lib/googleSheets.ts
+++ src/lib/googleSheets.ts
@@ -1368,7 +1368,7 @@ function insertExampleData() {
 function appendObjectAsRow(sheet, headers, obj) {
   var rowValues = headers.map(function(h) {
     var val = obj[h] !== undefined ? obj[h] : "";
-    if (typeof val === 'string' && /^[123]\/\d{4}$/.test(val)) {
+    if (typeof val === 'string' && /^[123]\\/\\d{4}$/.test(val)) {
       return "'" + val; // Prevent Google Sheets from formatting as Date
     }
     return val;
